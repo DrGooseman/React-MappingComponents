f6888a75c58f8c194f452530bee171380a0a60a7
DrGooseman <jamesjquinn12@gmail.com>
2020-02-20 22:56:14 +0300

    adds a form with hooks (just experimenting)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import emojipedia from "./../emojipedia";
 import Card from "./Card";
+import { useState } from "react";
 
 function mapCards(card) {
   return (
@@ -14,12 +15,66 @@ function mapCards(card) {
 }
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName(prevValue => ({ ...prevValue, [name]: value }));
+  }
+  function handleClick() {
+    // setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">{emojipedia.map(mapCards)}</dl>
+      <div>
+        <h1>
+          <span>emojipedia</span>
+        </h1>
+        <dl className="dictionary">{emojipedia.map(mapCards)}</dl>
+      </div>
+
+      <div className="container">
+        <h1>
+          Hello {fullName.fName} {fullName.lName}
+        </h1>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="First name."
+          name="fName"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Last name."
+          name="lName"
+          value={fullName.lName}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
